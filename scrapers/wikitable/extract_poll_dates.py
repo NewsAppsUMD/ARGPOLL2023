197b26d55ed88a59e68c2de4e9dc0b76f4b08e9b
Rmercad1 <113056985+Rmercad1@users.noreply.github.com>
2023-04-05 20:51:40 +0000

    test testy testing

--- scrapers/wikitable/extract_poll_dates.py
+++ scrapers/wikitable/extract_poll_dates.py
@@ -1,31 +1,23 @@
+import os
 import pandas as pd
 import datetime
-import os
 
+data_dir = os.getenv('DATA_DIR')
+file_path = os.path.join(data_dir, 'voting_intentions.csv')
 
-def extract_poll_dates(data_dir):
-    # Read the CSV file
-    df = pd.read_csv(os.path.join(data_dir, 'voting_intentions.csv'))
+df = pd.read_csv(file_path)
 
-    # Get the unique poll dates for the current month
-    curr_month = datetime.date.today().month
-    poll_dates = set()
+df['start_date'] = df['start_date'].apply(lambda x: datetime.datetime.strptime(x, '%d %B %Y').date())
+df['end_date'] = df['end_date'].apply(lambda x: datetime.datetime.strptime(x, '%d %B %Y').date())
 
-    for i, row in df.iterrows():
-        date_str = row['Fieldwork date']
-        if not pd.isna(date_str) and date_str != '':
-            date = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
-            if date.month == curr_month:
-                poll_dates.add(date.strftime('%Y-%m-%d'))
+curr_month = datetime.datetime.now().month
 
-    # Return the poll dates as a string
-    return ', '.join(sorted(list(poll_dates)))
+poll_dates = []
+for index, row in df.iterrows():
+    if row['start_date'].month == curr_month:
+        poll_dates.append(f"{row['start_date'].strftime('%B %d')} - {row['end_date'].strftime('%B %d, %Y')}")
 
-
-if __name__ == '__main__':
-    # Get the path to the data directory from the environment variable
-    data_dir = os.getenv('DATA_DIR')
-
-    # Extract the poll dates and print them to stdout
-    poll_dates = extract_poll_dates(data_dir)
-    print(f'::set-output name=poll_dates::{poll_dates}')
+if poll_dates:
+    print('\n'.join(poll_dates))
+else:
+    print("No polls this month.")
