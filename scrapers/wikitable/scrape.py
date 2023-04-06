c6575a58b3fb13302967ff772e5b762743927e6e
Rmercad1 <113056985+Rmercad1@users.noreply.github.com>
2023-04-06 20:48:55 +0000

    run job

--- scrapers/wikitable/scrape.py
+++ scrapers/wikitable/scrape.py
@@ -1,87 +1,66 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
 import pandas as pd
 import datetime
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.application import MIMEApplication
+import os
 
-# Get current month and year
-now = datetime.datetime.now()
-current_month = now.strftime("%B")
-current_year = now.strftime("%Y")
+# Define URL to scrape
+url = "https://en.wikipedia.org/wiki/Opinion_polling_for_the_2023_Argentine_general_election"
 
-url = "https://en.wikipedia.org/wiki/Opinion_polling_for_the_2023_Argentine_general_election#By_political_party_2023"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+# Send a request to the website and get the HTML content
+r = requests.get(url)
+soup = BeautifulSoup(r.content, "html.parser")
 
-table = soup.find_all('table')[1] # select the second table on the page
+# Find the table with the polling data
+table = soup.find("table", {"class": "wikitable"})
 
-rows = table.find_all('tr')
+# Find all rows of the table
+rows = table.findAll("tr")
 
-# extract table headers
-headers = []
-for th in rows[0].find_all('th'):
-    if th.find('a'):
-        headers.append(th.find('a')['title'])
-    else:
-        headers.append(th.text.strip())
+# Create empty lists to store the data
+dates = []
+polling_firms = []
+sample_sizes = []
+intents = []
 
-# find the index of the "Pollster" column and replace it with "Polling firm"
-pollster_index = headers.index("Polling firm")
-headers[pollster_index] = "Polling firm"
-
-# extract table data and filter for current month
-polling_data = []
+# Loop through the rows of the table and extract the data
 for row in rows[1:]:
-    row_data = []
-    date = row.find('th').text.strip()
-    date_obj = datetime.datetime.strptime(date, '%d %B %Y')
-    if date_obj.month == now.month and date_obj.year == now.year:
-        row_data.append(date)
-        for td in row.find_all('td'):
-            if td.find('a'):
-                row_data.append(td.find('a').get('title', td.text.strip()))
-            else:
-                row_data.append(td.text.strip())
-        polling_data.append(row_data)
+    cells = row.findAll("td")
+    date = cells[0].get_text().strip()
+    polling_firm = cells[1].get_text().strip()
+    sample_size = cells[2].get_text().strip()
+    intent = cells[3].get_text().strip()
 
-# convert polling data to pandas dataframe
-df = pd.DataFrame(polling_data, columns=headers)
+    # Check if the date is in the correct format
+    try:
+        date_obj = datetime.datetime.strptime(date, '%d %B %Y')
+    except ValueError:
+        print(f"Ignoring invalid date format: {date}")
+        continue
 
-# create HTML table
-html_table = df.to_html(index=False)
+    # Check if the date is in the current month
+    if date_obj.month != datetime.datetime.now().month:
+        continue
 
-# set up email
-msg = MIMEMultipart()
-msg['From'] = 'mercado@ryans.com'
-msg['To'] = 'mercado@ryans.com'
-msg['Subject'] = 'Daily Voting Intentions'
+    dates.append(date)
+    polling_firms.append(polling_firm)
+    sample_sizes.append(sample_size)
+    intents.append(intent)
 
-# create email body
-body = 'Hello,\n\nEnclosed you will find the latest polling results for the 2023 Argentine General Election.'
+# Create a dictionary to store the data
+data = {
+    "Date": dates,
+    "Polling Firm": polling_firms,
+    "Sample Size": sample_sizes,
+    "Intent": intents,
+}
 
-# attach HTML table to email
-part = MIMEText(html_table, 'html')
-msg.attach(part)
+# Create a pandas DataFrame from the dictionary
+df = pd.DataFrame(data)
 
-# attach CSV file to email
-with open('voting_intentions.csv', mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(headers)
-    writer.writerows(polling_data)
+# Create a directory to store the CSV file
+if not os.path.exists("scrapers/wikitable"):
+    os.makedirs("scrapers/wikitable")
 
-with open('voting_intentions.csv', 'rb') as f:
-    attach = MIMEApplication(f.read(),_subtype="csv")
-    attach.add_header('Content-Disposition','attachment',filename=str(current_month)+'_voting_intentions.csv')
-    msg.attach(attach)
-
-# send email
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login('mercado@ryans.com', 'password')
-text = msg.as_string()
-server.sendmail(msg['From'], msg['To'], text)
-server.quit()
+# Save the DataFrame to a CSV file
+df.to_csv("scrapers/wikitable/voting_intentions.csv", index=False)
