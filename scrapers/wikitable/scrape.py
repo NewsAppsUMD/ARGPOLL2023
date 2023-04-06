bd5b7f826d80107f5002ba8028298a0e7549186c
Rmercad1 <113056985+Rmercad1@users.noreply.github.com>
2023-04-06 20:29:06 +0000

    testing

--- scrapers/wikitable/scrape.py
+++ scrapers/wikitable/scrape.py
@@ -1,10 +1,19 @@
+import os
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
-import csv
-import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import smtplib
 
+# set up email parameters
+EMAIL = os.environ.get('EMAIL')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
+# scrape polling data from Wikipedia
 url = "https://en.wikipedia.org/wiki/Opinion_polling_for_the_2023_Argentine_general_election#By_political_party_2023"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
@@ -22,11 +31,8 @@ for th in rows[0].find_all('th'):
         headers.append(th.text.strip())
 
 # find the index of the "Pollster" column and replace it with "Polling firm"
-try:
-    pollster_index = headers.index("Pollster")
-    headers[pollster_index] = "Polling firm"
-except ValueError:
-    print("Error: 'Pollster' column not found in table headers.")
+pollster_index = headers.index("Pollster")
+headers[pollster_index] = "Polling firm"
 
 # create dictionary to store poll results
 polls = {}
@@ -34,11 +40,7 @@ polls = {}
 # extract table data and update dictionary
 for row in rows[1:]:
     poll_date = row.find('th').text.strip()
-    try:
-        poll_date = datetime.strptime(poll_date, '%d %B %Y').date()
-    except ValueError:
-        print(f"Error: Unable to parse date '{poll_date}'")
-        continue
+    poll_date = datetime.strptime(poll_date, '%d %B %Y').date()
     if poll_date.month == datetime.today().month: # check if poll date is in current month
         if poll_date not in polls:
             polls[poll_date] = {header: '' for header in headers[1:]} # initialize poll results with empty strings
@@ -51,16 +53,10 @@ df = pd.DataFrame.from_dict(polls, orient='index')
 # format date column
 df.index = df.index.strftime('%d %B %Y')
 
-# send email with DataFrame
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email import encoders
-
 # create email message
 msg = MIMEMultipart()
 msg['Subject'] = 'Daily Voting Intentions'
-msg['From'] = os.environ.get('EMAIL')
+msg['From'] = 'your_email@example.com'
 msg['To'] = 'recipient_email@example.com'
 
 # create text version of DataFrame
@@ -72,13 +68,15 @@ body += df_text
 
 msg.attach(MIMEText(body, 'plain'))
 
+# attach DataFrame as CSV file
+csv_data = df.to_csv(index=True).encode('utf-8')
+part = MIMEBase('application', 'octet-stream')
+part.set_payload(csv_data)
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment', filename="poll_data.csv")
+msg.attach(part)
+
 # send email with smtplib
-import smtplib
-
-# add your email and password as environment variables or replace with your own values
-EMAIL = os.environ.get('EMAIL')
-EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-
 with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
     smtp.ehlo()
     smtp.starttls()
@@ -86,13 +84,7 @@ with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
 
     smtp.login(EMAIL, EMAIL_PASSWORD)
 
-    # attach DataFrame as CSV file
-    csv_data = df.to_csv(index=True).encode('utf-8')
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload(csv_data)
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition', 'attachment', filename="polling_results.csv")
-    msg.attach(part)
-
     # send email
-    smtp.sendmail(EMAIL, 'recipient_email@example.com', msg.as
+    smtp.sendmail(EMAIL, 'recipient_email@example.com', msg.as_string())
+
+print('Email sent!')
