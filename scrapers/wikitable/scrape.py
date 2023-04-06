a2b84b8c27c0e9f9b7102d5c9ebef011ad33ea04
Rmercad1 <113056985+Rmercad1@users.noreply.github.com>
2023-04-06 21:18:11 +0000

    test

--- scrapers/wikitable/scrape.py
+++ scrapers/wikitable/scrape.py
@@ -1,66 +1,64 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import datetime
-import os
+from datetime import datetime
 
-# Define URL to scrape
-url = "https://en.wikipedia.org/wiki/Opinion_polling_for_the_2023_Argentine_general_election"
+url = "https://en.wikipedia.org/wiki/Opinion_polling_for_the_2023_Argentine_general_election#By_political_party_2023"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
 
-# Send a request to the website and get the HTML content
-r = requests.get(url)
-soup = BeautifulSoup(r.content, "html.parser")
+table = soup.find_all('table')[1] # select the second table on the page
 
-# Find the table with the polling data
-table = soup.find("table", {"class": "wikitable"})
+rows = table.find_all('tr')
 
-# Find all rows of the table
-rows = table.findAll("tr")
+# extract table headers
+headers = []
+for th in rows[0].find_all('th'):
+    if th.find('a'):
+        headers.append(th.find('a')['title'])
+    else:
+        headers.append(th.text.strip())
 
-# Create empty lists to store the data
-dates = []
-polling_firms = []
-sample_sizes = []
-intents = []
+# find the index of the "Pollster" column and replace it with "Polling firm"
+pollster_index = headers.index("Polling firm")
+headers[pollster_index] = "Polling firm"
 
-# Loop through the rows of the table and extract the data
+# extract table data and write to pandas DataFrame
+data = []
 for row in rows[1:]:
-    cells = row.findAll("td")
-    date = cells[0].get_text().strip()
-    polling_firm = cells[1].get_text().strip()
-    sample_size = cells[2].get_text().strip()
-    intent = cells[3].get_text().strip()
+    if row.find('th'):
+        date_str = row.find('th').text.strip()
+        date = datetime.strptime(date_str, '%d %b %Y')
+    else:
+        if date.month == datetime.now().month:
+            row_data = [date_str]
+            for td in row.find_all('td'):
+                if td.find('a'):
+                    row_data.append(td.find('a').get('title', td.text.strip()))
+                else:
+                    row_data.append(td.text.strip())
+            data.append(row_data)
 
-    # Check if the date is in the correct format
-    try:
-        date_obj = datetime.datetime.strptime(date, '%d %B %Y')
-    except ValueError:
-        print(f"Ignoring invalid date format: {date}")
-        continue
+df = pd.DataFrame(data, columns=headers)
 
-    # Check if the date is in the current month
-    if date_obj.month != datetime.datetime.now().month:
-        continue
+# group by polling firm and date and aggregate results
+df['Date'] = pd.to_datetime(df['Date'], format='%d %b %Y')
+df = df.groupby(['Polling firm', 'Date']).agg({'Percentage': 'max'}).reset_index()
 
-    dates.append(date)
-    polling_firms.append(polling_firm)
-    sample_sizes.append(sample_size)
-    intents.append(intent)
+# create formatted table as string
+table_str = df.to_html(index=False)
 
-# Create a dictionary to store the data
-data = {
-    "Date": dates,
-    "Polling Firm": polling_firms,
-    "Sample Size": sample_sizes,
-    "Intent": intents,
-}
+# create email body
+body = f"""Hello,
 
-# Create a pandas DataFrame from the dictionary
-df = pd.DataFrame(data)
+Enclosed you will find the latest polling results for the 2023 Argentine General Election.
 
-# Create a directory to store the CSV file
-if not os.path.exists("scrapers/wikitable"):
-    os.makedirs("scrapers/wikitable")
+{table_str}
 
-# Save the DataFrame to a CSV file
-df.to_csv("scrapers/wikitable/voting_intentions.csv", index=False)
+Thank you,
+Your Name
+"""
+
+# write email body to file
+with open('email_body.txt', mode='w') as file:
+    file.write(body)
