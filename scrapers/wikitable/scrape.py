7239353a49b21cf1687d0e60caf2914e8ae55a50
Rmercad1 <113056985+Rmercad1@users.noreply.github.com>
2023-04-06 21:57:56 +0000

    ahh

--- scrapers/wikitable/scrape.py
+++ scrapers/wikitable/scrape.py
@@ -1,38 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
+import csv
 
-url = 'https://en.wikipedia.org/wiki/Opinion_polling_for_the_2023_Argentine_general_election'
+url = "https://en.wikipedia.org/wiki/Opinion_polling_for_the_2023_Argentine_general_election#By_political_party_2023"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
 
-page = requests.get(url)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-table = soup.find_all('table')[0]
+table = soup.find_all('table')[1] # select the second table on the page
 
 rows = table.find_all('tr')
 
-data = []
+# extract table headers
+headers = []
+for th in rows[0].find_all('th'):
+    if th.find('a'):
+        headers.append(th.find('a')['title'])
+    else:
+        headers.append(th.text.strip())
 
-for row in rows[1:]:
-    cols = row.find_all('td')
-    if len(cols) >= 3:
-        date_str = cols[0].get_text().strip()
-        if date_str:
-            date = datetime.strptime(date_str, '%d %b %Y')
-            firm = cols[1].get_text().strip()
-            result = cols[2].get_text().strip()
-            data.append([date, firm, result])
+# find the index of the "Pollster" column and replace it with "Polling firm"
+pollster_index = headers.index("Polling firm")
+headers[pollster_index] = "Polling firm"
 
-data.sort(key=lambda x: x[0], reverse=True)
+# write table headers to CSV file
+with open('voting_intentions.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(headers)
 
-with open('scrapers/wikitable/voting_intentions.csv', 'w') as f:
-    f.write('Date,Firm,Result\n')
-    for row in data:
-        f.write('{},{},{}\n'.format(row[0].strftime('%Y-%m-%d'), row[1], row[2]))
+# extract table data and write to CSV file
+with open('voting_intentions.csv', mode='a', newline='') as file:
+    writer = csv.writer(file)
 
-with open('email_body.txt', 'w') as f:
-    f.write('Hello,\n\nEnclosed you will find the latest polling results for the 2023 Argentine General Election.\n\n')
-    f.write('Date\tFirm\tResult\n')
-    for row in data:
-        f.write('{}\t{}\t{}\n'.format(row[0].strftime('%Y-%m-%d'), row[1], row[2]))
+    for row in rows[1:]:
+        if row.find('th'):
+            data = [row.find('th').text.strip()]
+        else:
+            data = []
+        for td in row.find_all('td'):
+            if td.find('a'):
+                data.append(td.find('a').get('title', td.text.strip()))
+            else:
+                data.append(td.text.strip())
+        writer.writerow(data)
