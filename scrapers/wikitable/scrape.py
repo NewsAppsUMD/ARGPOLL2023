a07bcf4b7f3dac5fe7974a7de910baf5c5e7afbd
Rmercad1 <113056985+Rmercad1@users.noreply.github.com>
2023-04-07 20:20:18 +0000

    test

--- scrapers/wikitable/scrape.py
+++ scrapers/wikitable/scrape.py
@@ -1,68 +1,44 @@
-#name: Daily Polling Results
+import requests
+from bs4 import BeautifulSoup
+import csv
 
-on:
-  schedule:
-    - cron: "0 12 * * *"
+url = "https://en.wikipedia.org/wiki/Opinion_polling_for_the_2023_Argentine_general_election#By_political_party_2023"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
 
-jobs:
-  scrape_data:
-    runs-on: ubuntu-latest
-    steps:
-      - uses: actions/checkout@v2
-      - name: Set up Python 3.9
-        uses: actions/setup-python@v2
-        with:
-          python-version: '3.9'
-      - name: Install dependencies
-        run: |
-          python -m pip install --upgrade pip
-          pip install beautifulsoup4
-          pip install pandas
-      - name: Scrape data
-        run: python scraper.py
-      - name: Upload data
-        uses: actions/upload-artifact@v2
-        with:
-          name: voting_intentions
-          path: ./scrapers/wikitable/voting_intentions.csv
-      - name: Send email
-        uses: dawidd6/action-send-mail@v3.3.0
-        with:
-          server_address: smtp.gmail.com
-          server_port: 587
-          username: ${{ secrets.EMAIL }}
-          password: ${{ secrets.EMAIL_PASSWORD }}
-          subject: "Daily Voting Intentions"
-          body: |
-            <html>
-              <body>
-                <p>Hello,</p>
-                <p>Enclosed you will find the latest polling results for the 2023 Argentine General Election:</p>
-                <table>
-                  <tr>
-                    <th>Polling firm</th>
-                    <th>Date</th>
-                    <th>Party A</th>
-                    <th>Party B</th>
-                    <th>Party C</th>
-                  </tr>
-                  <tbody>
-                    {% for row in data %}
-                    <tr>
-                      <td>{{ row.pollster }}</td>
-                      <td>{{ row.date }}</td>
-                      <td>{{ row.party_a }}</td>
-                      <td>{{ row.party_b }}</td>
-                      <td>{{ row.party_c }}</td>
-                    </tr>
-                    {% endfor %}
-                  </tbody>
-                </table>
-                <p>Best regards,</p>
-                <p>Ryan Mercado</p>
-              </body>
-            </html>
-          from: mercadoryan94@gmail.com
-          to: mercadoryan94@gmail.com
-          attachments: |
-            ./scrapers/wikitable/voting_intentions.csv
+table = soup.find_all('table')[1] # select the second table on the page
+
+rows = table.find_all('tr')
+
+# extract table headers
+headers = []
+for th in rows[0].find_all('th'):
+    if th.find('a'):
+        headers.append(th.find('a')['title'])
+    else:
+        headers.append(th.text.strip())
+
+# find the index of the "Pollster" column and replace it with "Polling firm"
+pollster_index = headers.index("Polling firm")
+headers[pollster_index] = "Polling firm"
+
+# write table headers to CSV file
+with open('voting_intentions.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(headers)
+
+# extract table data and write to CSV file
+with open('voting_intentions.csv', mode='a', newline='') as file:
+    writer = csv.writer(file)
+
+    for row in rows[1:]:
+        if row.find('th'):
+            data = [row.find('th').text.strip()]
+        else:
+            data = []
+        for td in row.find_all('td'):
+            if td.find('a'):
+                data.append(td.find('a').get('title', td.text.strip()))
+            else:
+                data.append(td.text.strip())
+        writer.writerow(data)
