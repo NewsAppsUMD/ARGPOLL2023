cdd2d3a67d03dd1ce0430080c57af2bc953ead15
Rmercad1 <113056985+Rmercad1@users.noreply.github.com>
2023-04-07 02:15:15 +0000

    this shit cray

--- scrapers/wikitable/scrape.py
+++ scrapers/wikitable/scrape.py
@@ -1,9 +1,7 @@
-import os
-os.environ['DISPLAY'] = ':0'
-
 import requests
 from bs4 import BeautifulSoup
 import csv
+from datetime import datetime, timedelta
 
 url = "https://en.wikipedia.org/wiki/Opinion_polling_for_the_2023_Argentine_general_election#By_political_party_2023"
 response = requests.get(url)
@@ -25,23 +23,32 @@ for th in rows[0].find_all('th'):
 pollster_index = headers.index("Polling firm")
 headers[pollster_index] = "Polling firm"
 
-# write table headers to CSV file
-with open('voting_intentions.csv', mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(headers)
-
-# extract table data and write to CSV file
-with open('voting_intentions.csv', mode='a', newline='') as file:
-    writer = csv.writer(file)
-
-    for row in rows[1:]:
-        if row.find('th'):
-            data = [row.find('th').text.strip()]
-        else:
-            data = []
+# extract table data and group by date
+poll_data = {}
+for row in rows[1:]:
+    if row.find('th'):
+        poll_date_str = row.find('th').text.strip()
+        poll_date = datetime.strptime(poll_date_str, '%d %b %Y')
+        if poll_date.date() < datetime.now().date() - timedelta(days=30): # only include polls from last 30 days
+            break
+        if poll_date not in poll_data:
+            poll_data[poll_date] = []
+        parties = {}
         for td in row.find_all('td'):
             if td.find('a'):
-                data.append(td.find('a').get('title', td.text.strip()))
-            else:
-                data.append(td.text.strip())
-        writer.writerow(data)
+                party_name = td.find('a').get('title', td.text.strip())
+                if party_name not in parties:
+                    parties[party_name] = 0
+                parties[party_name] += 1
+        poll_data[poll_date].append(parties)
+
+# write table data to CSV file
+with open('voting_intentions.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Poll Date'] + headers[2:])
+    for poll_date, parties in poll_data.items():
+        row = [poll_date.strftime('%d-%b-%Y')]
+        for party_name in headers[2:]:
+            count = sum(parties_data.get(party_name, 0) for parties_data in parties)
+            row.append(count)
+        writer.writerow(row)
